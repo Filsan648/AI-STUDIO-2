b5318b213cf069d3b859758d276c9cd0c9bf097f
Filsan648 <filsanfouad2002@gmail.com>
2025-08-11 15:06:36 +0300

    updte

--- Ai/expand.py
+++ Ai/expand.py
@@ -10,7 +10,7 @@ def expand(file_url,ratio):
 
  headers = {
   "Content-Type": "application/json",
-  "api_token":"dcb510d623b84758aa83414d754b5860",
+  "api_token":"",
 }
 
  response = requests.post(url, json=payload, headers=headers)
